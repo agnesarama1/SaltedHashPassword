4e66c0b0702b3a76616c4971668818592082c20c
agnesarama1 <agnesa.rama1@student.uni-pr.edu>
2024-05-15 21:02:11 +0200

    HomePage Done, Styled

--- HTML/home.php
+++ HTML/home.php
@@ -1,6 +1,22 @@
+
 <?php
 
+
+session_start();
+
+// Check if the logout button is clicked
+if (isset($_POST['logout'])) {
+    // Perform any necessary logout actions here
+
+    // Destroy the session and redirect  to  the login page or any other desired location
+    session_destroy();
+     header("Location: login.php");
+    exit();
+}
+
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,8 +24,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HomePage</title>
+    <link rel="stylesheet" href="../CSS/home.css">
+    <!-- <style>
+        body {
+            background-color: #708090; 
+            background-size: cover; 
+            background-repeat: no-repeat; 
+            background-attachment: fixed; 
+            margin: 0; 
+            padding: 0; 
+            height: 100vh;
+        }
+    </style> -->
 </head>
 <body>
-    
+<div class="back"></div>
+ <div><img style="width:10%;"src="">
+</div>
+<p style="margin-right: 15%">
+<img style = " width: 5% ; margin-left:15%" src = "">
+</p>
+<div class="container">
+            <h1 style="color:white;"> Greeting Message</h1>
+            <p style="color:white;  ">In this guide, we'll show you how to develop a simple web application for user management where passwords are stored 
+                in the database as "salted hash". This method enhances the security of password storage by using a unique salt 
+                for each user, increasing protection against various attacks.</p>
+            <!-- <p>This project is implemented by group 3 using HTML,
+                 CSS and PHP for saving your data and especially<br> your password as salted hash</p> -->
+            <h1></h1>
+         <button>   <a href="login.php" class="btn">Log Out </a> </button>
+    </div>
 </body>
 </html>
