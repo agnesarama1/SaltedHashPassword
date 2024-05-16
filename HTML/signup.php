dca78a2791fe6e25c75674a2193f39d12e0fc50e
agnesarama1 <agnesa.rama1@student.uni-pr.edu>
2024-05-16 20:28:38 +0200

    signup page changes

--- HTML/signup.php
+++ HTML/signup.php
@@ -1,15 +1,17 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIGNUP</title>
+  <title>LogInSignUP</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../CSS/signup.css">
 </head>
 <body>
-    
+    <div class="container">
 
-<div class="container">
-<?php
+    <?php
 include_once( 'connectDB.php');
 if(isset($_POST['submit'])){
   $name = mysqli_real_escape_string($conn, $_POST['name']);
@@ -76,8 +78,31 @@ else{
 ?>
 
 
-
-</div>
-
+        <h4 class="mb-3 pb-3">Sign Up </h4>
+        <form action="" method="POST">  
+           
+          <div class="form-group">
+            <input type="text" class="form-style" placeholder="Full Name" name="name">
+            <i class="input-icon uil uil-user"></i>
+          </div>
+          <div class="form-group mt-2">
+            <input type="email" class="form-style" placeholder="Email" name="email">
+            <i class="input-icon uil uil-at"></i>
+          </div>
+          <div class="form-group mt-2">
+            <input type="password" class="form-style" placeholder="Password" name="password">
+            <i class="input-icon uil uil-lock-alt"></i>
+          </div>
+          <div class="form-group mt-2">
+            <input type="password" class="form-style" placeholder="Confirm Password" name="confirm_pass">
+            <i class="input-icon uil uil-lock-alt"></i>
+          </div>
+          <input type="submit" name="submit" value="Sign Up"></input>
+          <p class='HaveAnAccount'>Already have an account?<a class="loginbutton"href="login.php"> Login.</p>
+          
+         
+        </form>
+      </div>
 </body>
 </html>
+      
